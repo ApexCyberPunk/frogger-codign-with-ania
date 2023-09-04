adfd13b37960d098f32e4a8ec522dac2eb54b25f
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-04 05:33:15 -0700

    Learning is fun!
    Yayyyy!

---
+++ from.js
@@ -0,0 +1,91 @@
+
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+const logleft = document.querySelectorAll('.log-left')
+let currentIndex = 76
+console.log(resultDisplay)
+const width = 9
+
+function moveFrog(e) {
+    // console.log('moved')
+    squares[currentIndex].classList.remove('frog')
+    
+switch(e.key) {
+    case 'ArrowLeft' : 
+    if (currentIndex % width !== 0)
+        currentIndex -= 1
+    break;
+    case "ArrowRight":
+    if (currentIndex % width < width - 1)
+    currentIndex += 1
+    break;
+    case 'ArrowUp' : 
+    if (currentIndex - width >= 0)
+    currentIndex -= width
+    break;
+    case "ArrowDown":
+    if (currentIndex + width < width * width)
+    currentIndex += width
+    break;
+}
+    squares[currentIndex].classList.add('frog')
+    // squares
+}
+
+document.addEventListener('keyup', moveFrog)
+
+function autoMoveLogs() {
+    logleft.forEach(logLeft => moveLogLeft(logLeft))
+}
+
+function moveLogLeft(logLeft) {
+    switch(true) {
+        case logLeft.classList.contains('l1') :
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break
+            case logLeft.classList.contains('l2') :
+                logLeft.classList.remove('l2')
+                logLeft.classList.add('l3')
+                break
+                case logLeft.classList.contains('l3') :
+                    logLeft.classList.remove('l3')
+                    logLeft.classList.add('l4')
+                    break
+                    case logLeft.classList.contains('l4') :
+                        logLeft.classList.remove('l4')
+                        logLeft.classList.add('l5')
+                        break
+                        case logLeft.classList.contains('l5') :
+                            logLeft.classList.remove('l5')
+                            logLeft.classList.add('l1')
+                            break
+    }
+}
+
+
+setInterval(autoMoveLogs, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
